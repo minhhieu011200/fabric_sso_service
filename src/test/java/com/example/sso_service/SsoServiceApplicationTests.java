e835ea8f9aad4ca517a6ad49a67b6d40f7ec7933
Minh Hieu <minhhieu011200@gmail.com>
2024-09-12 15:35:53 +0700

    first commit

---
+++ src/test/java/com/example/sso_service/SsoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sso_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
